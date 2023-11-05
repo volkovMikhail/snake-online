b95931e9061fa6e775325d46cb3dc769bac63d2f
volkovMikhail <volkov.work1@gmail.com>
2023-11-06 01:00:37 +0300

    chore: add config dep, get port from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { SnakeModule } from './snake/snake.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
+    ConfigModule.forRoot(),
     SnakeModule
   ]
 })
